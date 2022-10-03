5d79311a5c053f70ed4020381749af9cccf99a4b
Hafixta Technologies <pdh.usman007@gmail.com>
2022-10-03 10:17:52 +0500

    minor change

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'netmobilefix@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vgbfyvbowdjwfpuu'
+app.config['MAIL_PASSWORD'] = 'direrhsfycidlavr'
+
 
 mail = Mail(app)
 
