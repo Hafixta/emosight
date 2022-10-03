eae40030c81e0a0d80f7b3143af90e433a1b24b0
Hafixta Technologies <pdh.usman007@gmail.com>
2022-10-03 10:07:32 +0500

    minor change

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'netmobilefix@gmail.com'
-app.config['MAIL_PASSWORD'] = 'upwork1122'
+app.config['MAIL_PASSWORD'] = 'vgbfyvbowdjwfpuu'
 
 mail = Mail(app)
 
